61c71bad909ca8aa89bb8a052a890ae9bca97322
HagarMohamedGamal <hagar.mohamed.gamal@gmail.com>
2020-05-18 05:30:24 +0200

    add user crud operations

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('/users', 'UserController');
+Route::get('/login', 'Auth\LoginController@login');
+
+Route::get('/register', 'Auth\RegisterController@register');
