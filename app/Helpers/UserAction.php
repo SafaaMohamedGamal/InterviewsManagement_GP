9f279b3de1babae16341695b0376b6a68d11a89d
HagarMohamedGamal <hagar.mohamed.gamal@gmail.com>
2020-05-18 18:00:30 +0200

    simplify logic in user controller

---
+++ app/Helpers/UserAction.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Helpers;
+
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserAction
+{
+    public static function update($id, $req)
+    {
+        // $req = $request->only(['name', 'email', 'password']);
+        $user = User::find($id);
+        $user->update([
+            "name" => isset($req["name"]) ? $req["name"] : $user['name'],
+            "email" => isset($req["email"]) ? $req["email"] : $user['email'],
+            "password" => isset($req["password"]) ? Hash::make($req["password"]) : $user['password'],
+        ]);
+        return response()->json([
+            "data" => $user,
+            "status" => 200
+        ]);
+    }
+    public static function store($user)
+    {
+        $user["password"] = Hash::make($user["password"]);
+        User::create($user);
+        return response()->json([
+            "data" => $user,
+            "status" => 200
+        ]);
+    }
+}
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,39 +10,19 @@ use App\Http\Requests\User\UpdateUserRequest;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return User::all();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(StoreUserRequest $request)
     {
         $user = $request->only(['name', 'email', 'password']);
-        $user["password"] = Hash::make($user["password"]);
-        User::create($user);
-        return response()->json([
-            "data" => $user,
-            "status" => 200
-        ]);
+        return \App\Helpers\UserAction::store($user);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Request $request, $id)
     {
         $user = User::find($id);
@@ -52,34 +32,14 @@ class UserController extends Controller
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(UpdateUserRequest $request, $id)
     {
         $req = $request->only(['name', 'email', 'password']);
-        $user = User::find($id);
-        $user->update([
-            "name" => isset($req["name"]) ? $req["name"] : $user['name'],
-            "email" => isset($req["email"]) ? $req["email"] : $user['email'],
-            "password" => isset($req["password"]) ? Hash::make($req["password"]) : $user['password'],
-        ]);
-        return response()->json([
-            "data" => $user,
-            "status" => 200
-        ]);
+        return \App\Helpers\UserAction::update($id, $req);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy($id)
     {
         $user = User::destroy($id);
