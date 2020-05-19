09d854b94764871793ee1b5470246e3837f71c72
SafaaMohamedGamal <safaa.mohamed.gamal@gmail.com>
2020-05-19 05:51:02 +0200

    validating seekers and employees before create or update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,20 +3,23 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use App\Seeker;
 use Illuminate\Http\Request;
+use App\Http\Requests\User\StoreUserRequest;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\UserController;
+use App\Http\Resources\Seeker as SeekerResource;
 
 class RegisterController extends Controller
 {
     // to be modified
-    public function register(Request $request){
+    public function register(StoreUserRequest $request){
         $user = $request->only(['name', 'email', 'password']);
-        $user["password"] = Hash::make($user["password"]);
-        User::create($user);
-        return response()->json([
-            "data" => $user,
-            ]);
+        $user = \App\Helpers\UserAction::store($user);
+        $seeker = Seeker::create();
+        $seeker->user()->save($user);
+
+        return new SeekerResource($user);
     }
 }
