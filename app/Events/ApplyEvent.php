77f8ed1247efac80e77abb2b72340955f5f03f0d
osama-eltayar <osamaeltiar@yahoo.com>
2020-06-15 16:45:10 +0200

    fix bugs

---
+++ app/Events/ApplyEvent.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ApplyEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public $application_id ;
+    public $user;
+    public $job ;
+
+    public function __construct($data)
+    {
+        $this->application_id=$data['id'] ;
+        $this->user = $data['user_name'] ;
+        $this->job = $data['job'];
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
